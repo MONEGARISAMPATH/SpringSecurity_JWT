59caf2d8a496b51f0f201048b80b13e6488a796b
Your Name <your-email@example.com>
2025-03-12 21:17:15 +0530

    changed the class

--- src/main/java/com/SpringSecurity/Controller/StudentController.java
+++ src/main/java/com/SpringSecurity/Controller/StudentController.java
@@ -38,7 +38,6 @@ public class StudentController {
 		return studentService.findById(id);
 	}
 	
-	//added teh comment
 	@GetMapping("/allstudents")
 	@PreAuthorize("hasRole('STUDENT')")
 	@ResponseStatus(value=HttpStatus.OK)
